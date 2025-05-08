cd9143cf8ac97178c96782685a14d35fe5ecca31
SEONSEUNG <seonseung@kakao.com>
2025-05-08 14:57:14 +0900

    chapter8 1st update

---
+++ chapter8/chapter8/ex8-1test.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+/*
+class Calculator {
+public:
+	int x, y;
+	void set(int x, int y) { this->x = x; this->y = y; }
+};
+
+class Add : public Calculator {
+public:
+	void add(){
+		cout << "Add: " <<  x + y << endl;
+	}
+};
+
+class Multiply : public Calculator {
+public:
+	void multiply() {
+		cout << "Multiply: " << x * y << endl;
+	}
+};
+
+int main() {
+	Add a;
+	Multiply b;
+	a.set(3, 4);
+	b.set(3, 4);
+	a.add();
+	b.multiply();
+}
+*/
